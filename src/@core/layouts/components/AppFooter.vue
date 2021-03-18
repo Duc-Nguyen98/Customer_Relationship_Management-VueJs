ee8bdbfe299b27bb4caa952ffb9f5726b08c85d0
Nguyễn Quang Đức <52225998+Duc-Nguyen99@users.noreply.github.com>
2021-03-18 10:10:13 +0700

    change title
    change title

--- src/@core/layouts/components/AppFooter.vue
+++ src/@core/layouts/components/AppFooter.vue
@@ -1,22 +1,29 @@
 <template>
   <p class="clearfix mb-0">
-    <span class="float-md-left d-block d-md-inline-block mt-25">
-      COPYRIGHT  © {{ new Date().getFullYear() }}
-      <b-link
+    <b>
+      <span class="float-md-left d-block d-md-inline-block mt-25">Hand-crafted &amp; Made with <b-link
         class="ml-25"
-        href="https://1.envato.market/pixinvent_portfolio"
+        href="#"
         target="_blank"
-      >Pixinvent</b-link>
-      <span class="d-none d-sm-inline-block">, All rights Reserved</span>
-    </span>
-
-    <span class="float-md-right d-none d-md-block">Hand-crafted &amp; Made with
-      <feather-icon
-        icon="HeartIcon"
-        size="21"
-        class="text-danger stroke-current"
-      />
-    </span>
+      >ANT </b-link> Team
+        <feather-icon
+          icon="HeartIcon"
+          size="21"
+          class="text-danger stroke-current"
+        />
+      </span>
+    </b>
+    <b>
+      <span class="float-md-right d-none d-md-block">
+        COPYRIGHT  © {{ new Date().getFullYear() }}
+        <b-link
+          class="ml-25"
+          href="#"
+          target="_blank"
+        >CVV</b-link>
+        <span class="d-none d-sm-inline-block">, All rights Reserved</span>
+      </span>
+    </b>
   </p>
 </template>
 
