2dfdb77f920bf38675d27e73dc590dc44e451232
Nguyễn Quang Đức <52225998+Duc-Nguyen99@users.noreply.github.com>
2021-03-22 00:28:25 +0700

    config
    config

--- src/navigation/vertical/charts-and-maps.js
+++ src/navigation/vertical/charts-and-maps.js
@@ -8,23 +8,23 @@ export default [
     tag: '3',
     tagVariant: 'light-danger',
     children: [
-      {
-        title: 'Apex Chart',
-        route: 'charts-apex-chart',
-      },
-      {
-        title: 'Chartjs',
-        route: 'charts-chartjs',
-      },
-      {
-        title: 'Echart',
-        route: 'charts-echart',
-      },
+      // {
+      //   title: 'Apex Chart',
+      //   route: 'charts-apex-chart',
+      // },
+      // {
+      //   title: 'Chartjs',
+      //   route: 'charts-chartjs',
+      // },
+      // {
+      //   title: 'Echart',
+      //   route: 'charts-echart',
+      // },
     ],
   },
-  {
-    title: 'Leaflet',
-    route: 'maps-leaflet',
-    icon: 'MapIcon',
-  },
+  // {
+  //   title: 'Leaflet',
+  //   route: 'maps-leaflet',
+  //   icon: 'MapIcon',
+  // },
 ]
