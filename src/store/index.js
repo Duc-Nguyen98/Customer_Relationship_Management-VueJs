63237d6ab168a015da447cfecd501c9ce7c55a31
Nguyễn Quang Đức <52225998+Duc-Nguyen99@users.noreply.github.com>
2021-03-25 22:47:12 +0700

    bug
    bug

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 // Modules
-import ecommerceStoreModule from '@/views/apps/e-commerce/eCommerceStoreModule'
 import app from './app'
 import appConfig from './app-config'
 import verticalMenu from './vertical-menu'
@@ -14,7 +13,6 @@ export default new Vuex.Store({
     app,
     appConfig,
     verticalMenu,
-    'app-ecommerce': ecommerceStoreModule,
   },
   strict: process.env.DEV,
 })
