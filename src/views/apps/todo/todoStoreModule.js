5d87feca9b1ff9e4f21f69277b1039ef60a52f32
Nguyễn Quang Đức <52225998+Duc-Nguyen99@users.noreply.github.com>
2021-03-26 02:24:56 +0700

    conffig
    conffig

--- src/views/apps/todo/todoStoreModule.js
+++ src/views/apps/todo/todoStoreModule.js
@@ -1,4 +1,4 @@
-import axios from '@axios'
+import axios from "@axios";
 
 export default {
   namespaced: true,
@@ -9,34 +9,34 @@ export default {
     fetchTasks(ctx, payload) {
       return new Promise((resolve, reject) => {
         axios
-          .get('/apps/todo/tasks', { params: payload })
-          .then(response => resolve(response))
-          .catch(error => reject(error))
-      })
+          .get("/apps/todo/tasks", { params: payload })
+          .then((response) => resolve(response))
+          .catch((error) => reject(error));
+      });
     },
     addTask(ctx, taskData) {
       return new Promise((resolve, reject) => {
         axios
-          .post('/apps/todo/tasks', { task: taskData })
-          .then(response => resolve(response))
-          .catch(error => reject(error))
-      })
+          .post("/apps/todo/tasks", { task: taskData })
+          .then((response) => resolve(response))
+          .catch((error) => reject(error));
+      });
     },
     updateTask(ctx, { task }) {
       return new Promise((resolve, reject) => {
         axios
           .post(`/apps/todo/tasks/${task.id}`, { task })
-          .then(response => resolve(response))
-          .catch(error => reject(error))
-      })
+          .then((response) => resolve(response))
+          .catch((error) => reject(error));
+      });
     },
     removeTask(ctx, { id }) {
       return new Promise((resolve, reject) => {
         axios
           .delete(`/apps/todo/tasks/${id}`)
-          .then(response => resolve(response))
-          .catch(error => reject(error))
-      })
+          .then((response) => resolve(response))
+          .catch((error) => reject(error));
+      });
     },
   },
-}
+};
