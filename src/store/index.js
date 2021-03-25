a0c38a3011f31dd9e750840597fdc07885a27bd8
Nguyễn Quang Đức <52225998+Duc-Nguyen99@users.noreply.github.com>
2021-03-25 23:01:38 +0700

    config folder project
    config folder project

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,6 @@ import Vuex from 'vuex'
 
 // Modules
 import app from './app'
-import appConfig from './app-config'
 import verticalMenu from './vertical-menu'
 
 Vue.use(Vuex)
@@ -11,7 +10,6 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   modules: {
     app,
-    appConfig,
     verticalMenu,
   },
   strict: process.env.DEV,
