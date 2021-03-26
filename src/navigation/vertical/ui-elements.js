7993d833f55dce0a89cd76dc5a59680da07e75fe
Nguyễn Quang Đức <52225998+Duc-Nguyen99@users.noreply.github.com>
2021-03-26 21:35:47 +0700

    config router

--- src/navigation/vertical/ui-elements.js
+++ src/navigation/vertical/ui-elements.js
@@ -1,6 +1,6 @@
 export default [
   {
-    header: 'User Interface',
+    header: 'User Elements',
   },
   {
     title: 'Typography',
@@ -227,6 +227,259 @@ export default [
       {
         title: 'I18n',
         route: 'extensions-i18n',
+      },
+      {
+        title: 'Forms Elements',
+        icon: 'CopyIcon',
+        children: [
+          {
+            title: 'Input',
+            route: 'forms-element-input',
+          },
+          {
+            title: 'Input Group',
+            route: 'forms-element-input-group',
+          },
+          {
+            title: 'Input Mask',
+            route: 'forms-element-input-mask',
+          },
+          {
+            title: 'Textarea',
+            route: 'forms-element-textarea',
+          },
+          {
+            title: 'Auto Suggest',
+            route: 'extensions-auto-suggest',
+          },
+          {
+            title: 'Checkbox',
+            route: 'forms-element-checkbox',
+          },
+          {
+            title: 'Radio',
+            route: 'forms-element-radio',
+          },
+          {
+            title: 'Switch',
+            route: 'forms-element-switch',
+          },
+          {
+            title: 'Select',
+            route: 'forms-element-select',
+          },
+          {
+            title: 'Vue Select',
+            route: 'extensions-vue-select',
+          },
+          {
+            title: 'Spinbutton',
+            route: 'forms-element-spinbutton',
+          },
+          {
+            title: 'File Input',
+            route: 'forms-element-file-input',
+          },
+          {
+            title: 'Quill Editor',
+            route: 'extensions-quill-editor',
+          },
+          {
+            title: 'Form Datepicker',
+            route: 'forms-element-datepicker',
+          },
+          {
+            title: 'Form Timepicker',
+            route: 'forms-element-timepicker',
+          },
+          {
+            title: 'Date Time Picker',
+            route: 'extensions-date-time-picker',
+          },
+          {
+            title: 'Form Rating',
+            route: 'forms-element-rating',
+          },
+          {
+            title: 'Form Tag',
+            route: 'forms-element-tag',
+          },
+        ],
+      },
+      {
+        title: 'Form Layout',
+        route: 'form-layout',
+        icon: 'CopyIcon',
+      },
+      {
+        title: 'Form Wizard',
+        route: 'form-wizard',
+        icon: 'PackageIcon',
+      },
+      {
+        title: 'Form Validation',
+        route: 'form-validation',
+        icon: 'CheckCircleIcon',
+      },
+      {
+        title: 'Form Repeater',
+        route: 'form-repeater',
+        icon: 'CopyIcon',
+      },
+      {
+        title: 'BS Table',
+        route: 'table-bs-table',
+        icon: 'ServerIcon',
+      },
+      {
+        title: 'Good Table',
+        route: 'table-good-table',
+        icon: 'GridIcon',
+      },
+
+    ],
+  },
+  {
+    title: 'Pages',
+    icon: 'FileIcon',
+    children: [
+      {
+        title: 'Authentication',
+        icon: 'CircleIcon',
+        children: [
+          {
+            title: 'Login v1',
+            route: 'auth-login-v1',
+            target: '_blank',
+          },
+          {
+            title: 'Login v2',
+            route: 'auth-login-v2',
+            target: '_blank',
+          },
+          {
+            title: 'Register v1',
+            route: 'auth-register-v1',
+            target: '_blank',
+          },
+          {
+            title: 'Register v2',
+            route: 'auth-register-v2',
+            target: '_blank',
+          },
+          {
+            title: 'Forgot Password v1',
+            route: 'auth-forgot-password-v1',
+            target: '_blank',
+          },
+          {
+            title: 'Forgot Password v2',
+            route: 'auth-forgot-password-v2',
+            target: '_blank',
+          },
+          {
+            title: 'Reset Password v1',
+            route: 'auth-reset-password-v1',
+            target: '_blank',
+          },
+          {
+            title: 'Reset Password v2',
+            route: 'auth-reset-password-v2',
+            target: '_blank',
+          },
+        ],
+      },
+      {
+        title: 'Account Settings',
+        route: 'pages-account-setting',
+      },
+      {
+        title: 'Profile',
+        route: 'pages-profile',
+      },
+      {
+        title: 'Faq',
+        route: 'pages-faq',
+      },
+      {
+        title: 'Knowledge Base',
+        route: 'pages-knowledge-base',
+      },
+      {
+        title: 'Pricing',
+        route: 'pages-pricing',
+      },
+      {
+        title: 'Blog',
+        children: [
+          {
+            title: 'List',
+            route: 'pages-blog-list',
+          },
+          {
+            title: 'Detail',
+            route: { name: 'pages-blog-detail', params: { id: 1 } },
+          },
+          {
+            title: 'Edit',
+            route: { name: 'pages-blog-edit', params: { id: 1 } },
+          },
+        ],
+      },
+      {
+        title: 'Mail Templates',
+        children: [
+          {
+            title: 'Welcome',
+            href: 'https://pixinvent.com/demo/vuexy-mail-template/mail-welcome.html',
+          },
+          {
+            title: 'Reset Password',
+            href: 'https://pixinvent.com/demo/vuexy-mail-template/mail-reset-password.html',
+          },
+          {
+            title: 'Verify Email',
+            href: 'https://pixinvent.com/demo/vuexy-mail-template/mail-verify-email.html',
+          },
+          {
+            title: 'Deactivate Account',
+            href: 'https://pixinvent.com/demo/vuexy-mail-template/mail-deactivate-account.html',
+          },
+          {
+            title: 'Invoice',
+            href: 'https://pixinvent.com/demo/vuexy-mail-template/mail-invoice.html',
+          },
+          {
+            title: 'Promotional',
+            href: 'https://pixinvent.com/demo/vuexy-mail-template/mail-promotional.html',
+          },
+        ],
+      },
+      {
+        title: 'Miscellaneous',
+        icon: 'CircleIcon',
+        children: [
+          {
+            title: 'Coming Soon',
+            route: 'misc-coming-soon',
+            target: '_blank',
+          },
+          {
+            title: 'Not Authorized',
+            route: 'misc-not-authorized',
+            target: '_blank',
+          },
+          {
+            title: 'Under Maintenance',
+            route: 'misc-under-maintenance',
+            target: '_blank',
+          },
+          {
+            title: 'Error',
+            route: 'misc-error',
+            target: '_blank',
+          },
+        ],
       },
     ],
   },
