252fb6d5bacf2b853e96927e440b14061fdc1a36
Nguyễn Quang Đức <52225998+Duc-Nguyen99@users.noreply.github.com>
2021-03-27 12:11:10 +0700

    Update AnalyticsAvgSessions.vue

--- src/views/dashboard/analytics/AnalyticsAvgSessions.vue
+++ src/views/dashboard/analytics/AnalyticsAvgSessions.vue
@@ -1,7 +1,6 @@
 <template>
   <b-card v-if="data">
     <b-row class="pb-50">
-
       <!-- text and button -->
       <b-col
         sm="6"
@@ -19,7 +18,7 @@
 
           <h5 class="font-medium-2">
             <span class="text-success mr-50">{{ data.growth }}</span>
-            <span>vs last 7 days</span>
+            <span>vs to last weeks</span>
           </h5>
         </div>
 
@@ -50,10 +49,7 @@
             no-caret
             size="sm"
           >
-            <b-dropdown-item
-              v-for="day in data.lastDays"
-              :key="day"
-            >
+            <b-dropdown-item v-for="day in data.lastDays" :key="day">
               {{ day }}
             </b-dropdown-item>
           </b-dropdown>
@@ -68,36 +64,19 @@
         />
       </b-col>
     </b-row>
-    <hr>
+    <hr />
 
     <!-- progress bar -->
     <b-row class="avg-sessions pt-50">
-      <b-col
-        cols="6"
-        class="mb-2"
-      >
-        <b-card-text class="mb-50">
-          Goal: ${{ data.goal }}
-        </b-card-text>
-        <b-progress
-          value="50"
-          max="100"
-          height="6px"
-        />
+      <b-col cols="6" class="mb-2">
+        <b-card-text class="mb-50"> Goal: ${{ data.goal }} </b-card-text>
+        <b-progress value="50" max="100" height="6px" />
       </b-col>
-      <b-col
-        cols="6"
-        class="mb-2"
-      >
+      <b-col cols="6" class="mb-2">
         <b-card-text class="mb-50">
           Users: {{ kFormatter(data.users) }}
         </b-card-text>
-        <b-progress
-          value="60"
-          max="100"
-          height="6px"
-          variant="warning"
-        />
+        <b-progress value="60" max="100" height="6px" variant="warning" />
       </b-col>
       <b-col cols="6">
         <b-card-text class="mb-50">
@@ -129,12 +108,19 @@
 
 <script>
 import {
-  BCard, BRow, BCol, BButton, BDropdown, BDropdownItem, BProgress, BCardText,
-} from 'bootstrap-vue'
-import VueApexCharts from 'vue-apexcharts'
-import Ripple from 'vue-ripple-directive'
-import { $themeColors } from '@themeConfig'
-import { kFormatter } from '@core/utils/filter'
+  BCard,
+  BRow,
+  BCol,
+  BButton,
+  BDropdown,
+  BDropdownItem,
+  BProgress,
+  BCardText,
+} from "bootstrap-vue";
+import VueApexCharts from "vue-apexcharts";
+import Ripple from "vue-ripple-directive";
+import { $themeColors } from "@themeConfig";
+import { kFormatter } from "@core/utils/filter";
 
 export default {
   components: {
@@ -174,36 +160,36 @@ export default {
           },
           states: {
             hover: {
-              filter: 'none',
+              filter: "none",
             },
           },
           colors: [
-            '#ebf0f7',
-            '#ebf0f7',
+            "#ebf0f7",
+            "#ebf0f7",
             $themeColors.primary,
-            '#ebf0f7',
-            '#ebf0f7',
-            '#ebf0f7',
+            "#ebf0f7",
+            "#ebf0f7",
+            "#ebf0f7",
           ],
           plotOptions: {
             bar: {
-              columnWidth: '45%',
+              columnWidth: "45%",
               distributed: true,
-              endingShape: 'rounded',
+              endingShape: "rounded",
             },
           },
           tooltip: {
             x: { show: false },
           },
           xaxis: {
-            type: 'numeric',
+            type: "numeric",
           },
         },
       },
-    }
+    };
   },
   methods: {
     kFormatter,
   },
-}
+};
 </script>
