e1ef56683dacd638d46c21511484f05afcfc8c6e
Nguyễn Quang Đức <52225998+Duc-Nguyen99@users.noreply.github.com>
2021-04-04 18:55:37 +0700

    Update forms-and-table.js

--- src/navigation/vertical/forms-and-table.js
+++ src/navigation/vertical/forms-and-table.js
@@ -4,20 +4,25 @@ export default [
   },
   {
     title: 'Table Customers',
+    route: 'apps-users-list',
     icon: 'UserIcon',
-    children: [
-      {
-        title: 'List',
-        route: 'apps-users-list',
-      },
-      {
-        title: 'View',
-        route: { name: 'apps-users-view', params: { id: 21 } },
-      },
-      {
-        title: 'Add',
-        route: { name: 'apps-users-add'},
-      },
-    ],
   },
+  // {
+  //   title: 'Table Customers',
+  //   icon: 'UserIcon',
+  //   children: [
+  //     {
+  //       title: 'List',
+  //       route: 'apps-users-list',
+  //     },
+  //     {
+  //       title: 'View',
+  //       route: { name: 'apps-users-view', params: { id: 21 } },
+  //     },
+  //     {
+  //       title: 'Add',
+  //       route: { name: 'apps-users-add' },
+  //     },
+  //   ],
+  // },
 ]
