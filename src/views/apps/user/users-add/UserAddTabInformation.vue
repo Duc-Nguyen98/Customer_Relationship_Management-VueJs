4dec8917906d75ca3e460c2d6f080132f9734824
Nguyễn Quang Đức <52225998+Duc-Nguyen99@users.noreply.github.com>
2021-04-04 18:09:05 +0700

    Update UserAddTabInformation.vue

--- src/views/apps/user/users-add/UserAddTabInformation.vue
+++ src/views/apps/user/users-add/UserAddTabInformation.vue
@@ -1,247 +1,242 @@
 <template>
   <div>
-    <!-- Header: Personal Info -->
-    <div class="d-flex">
-      <feather-icon icon="UserIcon" size="19" />
-      <h4 class="mb-0 ml-50">Personal Information</h4>
-    </div>
+    <validation-observer ref="simpleRules">
+      <b-form class="mt-1">
+        <!-- Header: Personal Info -->
+        <div class="d-flex mb-2">
+          <feather-icon icon="UserIcon" size="19" />
+          <h4 class="mb-0 ml-50">Personal Information</h4>
+        </div>
+        <b-row>
+          <!--  Full Name-->
+          <b-col cols="12" md="6" lg="4">
+            <b-form-group>
+              <label>Full Name</label>
+              <validation-provider
+                #default="{ errors }"
+                rules="required"
+                name="Full Name"
+              >
+                <b-form-input
+                  v-model="blankData.C_Name"
+                  :state="errors.length > 0 ? false : null"
+                  placeholder="Enter Full Name"
+                />
+                <small class="text-danger">{{ errors[0] }}</small>
+              </validation-provider>
+            </b-form-group>
+          </b-col>
 
-    <!-- Form: Personal Info Form -->
-    <b-form class="mt-1">
-      <b-row>
-        <!-- Field: Name  -->
-        <b-col cols="12" md="6" lg="4">
-          <b-form-group label="Full Name" label-for="Full Name">
-            <b-form-input
-              id="fullname"
-              v-model="userDataInfo.fullname"
-              placeholder="Full Name"
-            />
-          </b-form-group>
-        </b-col>
-        <!-- Field: Email -->
-        <b-col cols="12" md="6" lg="4">
-          <b-form-group label="Email" label-for="Email">
-            <b-form-input
-              id="email"
-              type="email"
-              v-model="userDataInfo.email"
-              placeholder="Email"
-            />
-          </b-form-group>
-        </b-col>
-        <!-- Field: Adress  -->
-        <b-col cols="12" md="6" lg="4">
-          <b-form-group label="Adress" label-for="Adress">
-            <b-form-input
-              id="adress"
-              v-model="userDataInfo.adress"
-              placeholder="Adress"
-            />
-          </b-form-group>
-        </b-col>
-        <!-- Field: Birth Date -->
-        <b-col cols="12" md="6" lg="4">
-          <b-form-group label="Birth Date" label-for="birth-date">
-            <flat-pickr
-              v-model="userDataInfo.dob"
-              class="form-control"
-              :config="{ dateFormat: 'Y-m-d' }"
-              placeholder="YYYY-MM-DD"
-            />
-          </b-form-group>
-        </b-col>
+          <!-- email -->
+          <b-col cols="12" md="6" lg="4">
+            <b-form-group>
+              <label>Email</label>
+              <validation-provider
+                #default="{ errors }"
+                name="Email"
+                rules="required|email"
+              >
+                <b-form-input
+                  v-model="blankData.C_Email"
+                  :state="errors.length > 0 ? false : null"
+                  placeholder="Email"
+                />
+                <small class="text-danger">{{ errors[0] }}</small>
+              </validation-provider>
+            </b-form-group>
+          </b-col>
 
-        <!-- Field: Mobile -->
-        <b-col cols="12" md="6" lg="4">
-          <b-form-group label="Mobile" label-for="mobile">
-            <b-form-input
-              id="mobile"
-              v-model="userDataInfo.mobile"
-              placeholder="+84303177289"
-            />
-          </b-form-group>
-        </b-col>
+          <!-- Adress -->
+          <b-col cols="12" md="6" lg="4">
+            <b-form-group>
+              <label>Adress</label>
+              <validation-provider
+                #default="{ errors }"
+                name="Adress"
+                rules="required|"
+              >
+                <b-form-input
+                  v-model="blankData.C_Adress"
+                  :state="errors.length > 0 ? false : null"
+                  placeholder="Adress"
+                />
+                <small class="text-danger">{{ errors[0] }}</small>
+              </validation-provider>
+            </b-form-group>
+          </b-col>
 
-        <!-- Field: Gender -->
-        <b-col cols="12" md="6" lg="4">
-          <b-form-group label="Gender" label-for="gender" label-class="mb-1">
+          <!-- Birth Day -->
+          <b-col cols="12" md="6" lg="4">
+            <b-form-group>
+              <label for="datepicker-placeholder">Birth Date</label>
+              <b-form-datepicker
+                id="datepicker-placeholder"
+                placeholder="Choose a date"
+                local="vi"
+                v-model="blankData.C_DoB"
+              />
+            </b-form-group>
+          </b-col>
+
+          <!-- Telephone Number -->
+          <b-col cols="12" md="6" lg="4">
+            <b-form-group>
+              <label>Telephone Number</label>
+              <validation-provider
+                #default="{ errors }"
+                rules="required|regex:^([0-9]+)$|digits:10"
+                name="Telephone Number"
+              >
+                <b-form-input
+                  v-model="blankData.C_Phone"
+                  :state="errors.length > 0 ? false : null"
+                  placeholder="Enter Telephone Number"
+                />
+                <small class="text-danger">{{ errors[0] }}</small>
+              </validation-provider>
+            </b-form-group>
+          </b-col>
+
+          <!--  Full Name-->
+          <b-col cols="12" md="6" lg="4">
             <b-form-radio-group
-              id="gender"
-              v-model="userDataInfo.gender"
+              v-model="genderOptions.value"
               :options="genderOptions"
-              value="male"
-            />
-          </b-form-group>
-        </b-col>
-      </b-row>
-
-      <!-- Header: Personal Info -->
-      <div class="d-flex mt-2">
-        <feather-icon icon="MapPinIcon" size="19" />
-        <h4 class="mb-0 ml-50">Address</h4>
-      </div>
-
-      <!-- Form: Personal Info Form -->
-      <b-row class="mt-1">
-        <!-- Field: City -->
-        <b-col cols="12" md="6" lg="4">
-          <b-form-group label="Province Name" label-for="city">
-            <b-form-select v-model="selected" :options="options" />
-            <!-- <b-form-input id="city" v-model="userDataInfo.city" /> -->
-          </b-form-group>
-        </b-col>
-
-        <!-- Field: State -->
-        <b-col cols="12" md="6" lg="4">
-          <b-form-group label="District Name" label-for="state">
-            <b-form-select
-              v-model="selected"
-              :options="options"
-            ></b-form-select>
-          </b-form-group>
-        </b-col>
-
-        <!-- Field: Country -->
-        <b-col cols="12" md="6" lg="4">
-          <b-form-group label="Ward Name" label-for="country">
-            <b-form-select
-              v-model="selected"
-              :options="options"
-            ></b-form-select>
-          </b-form-group>
-        </b-col>
-      </b-row>
-
-      <!-- Header: Personal Info -->
-      <div class="d-flex mt-2">
-        <feather-icon icon="MapPinIcon" size="19" />
-        <h4 class="mb-0 ml-50">Note</h4>
-      </div>
-
-      <!-- Form: Personal Info Form -->
-      <b-row class="mt-1">
-        <!-- Field: City -->
-        <b-col cols="12" md="12" lg="12">
-          <b-form-group label="" label-for="city">
-            <b-form-textarea
-              id="textarea-rows"
-              placeholder="Note Here"
-              rows="8"
-            />
-          </b-form-group>
-        </b-col>
-      </b-row>
-
-      <!-- Form Actions -->
-      <div class="d-flex float-right mt-2">
-        <b-button
-          v-ripple.400="'rgba(186, 191, 199, 0.15)'"
-          type="button"
-          variant="outline-secondary"
-          class="mr-2 text-uppercase"
-        >
-          Cancel
-        </b-button>
-
-        <b-button
-          v-ripple.400="'rgba(186, 191, 199, 0.15)'"
-          type="button"
-          variant="primary"
-          class="mr-2 text-up text-uppercase"
-        >
-          Reset
-        </b-button>
-
-        <b-button
-          v-ripple.400="'rgba(255, 255, 255, 0.15)'"
-          variant="primary"
-          type="submit"
-          class="text-uppercase"
-        >
-          Add New
-        </b-button>
-      </div>
-    </b-form>
+              class="demo-inline-spacing"
+              name="radio-validation"
+              value="Male"
+            >
+            </b-form-radio-group>
+          </b-col>
+        </b-row>
+        <!-- Header: Personal Select -->
+        <div class="d-flex my-2">
+          <feather-icon icon="MapPinIcon" size="19" />
+          <h4 class="mb-0 ml-50">Address</h4>
+        </div>
+        <b-row>
+          <!--  Province Name-->
+          <b-col cols="12" md="6" lg="4">
+            <b-form-group>
+              <label>Province Name</label>
+              <b-form-select
+                v-model="objSelectProvince.selected"
+                :options="objSelectProvince.options"
+              />
+            </b-form-group>
+          </b-col>
+          <!--  District Name-->
+          <b-col cols="12" md="6" lg="4">
+            <b-form-group>
+              <label>District Name</label>
+              <b-form-select
+                v-model="objSelectDistrict.selected"
+                :options="objSelectDistrict.options"
+              />
+            </b-form-group>
+          </b-col>
+          <!--  Ward Name-->
+          <b-col cols="12" md="6" lg="4">
+            <b-form-group>
+              <label>Ward Name</label>
+              <b-form-select
+                v-model="objSelectWard.selected"
+                :options="objSelectWard.options"
+              />
+            </b-form-group>
+          </b-col>
+        </b-row>
+        <!-- Header: Personal Note -->
+        <div class="d-flex my-2">
+          <feather-icon icon="MapPinIcon" size="19" />
+          <h4 class="mb-0 ml-50">Note</h4>
+        </div>
+        <b-row>
+          <!--  Note -->
+          <b-col cols="12" md="12" lg="12">
+            <b-form-group label="Note Information" label-for="Note Information">
+              <b-form-textarea
+                id="textarea-rows"
+                placeholder="Note Here..."
+                rows="8"
+                v-model="blankData.C_Note"
+              />
+            </b-form-group>
+          </b-col>
+        </b-row>
+        <!-- submit button -->
+        <b-row>
+          <b-col cols="12">
+            <b-button
+              variant="primary"
+              type="submit"
+              @click.prevent="validationForm"
+            >
+              Submit
+            </b-button>
+          </b-col>
+        </b-row>
+      </b-form>
+    </validation-observer>
   </div>
 </template>
 
 <script>
+import { ValidationProvider, ValidationObserver } from "vee-validate";
 import {
+  BFormTextarea,
+  BFormDatepicker,
   BFormSelect,
+  BFormRadioGroup,
+  BFormInput,
+  BFormGroup,
+  BForm,
   BRow,
   BCol,
-  BForm,
-  BFormTextarea,
-  BFormGroup,
-  BFormInput,
-  BFormRadioGroup,
-  BFormCheckboxGroup,
   BButton,
 } from "bootstrap-vue";
-import flatPickr from "vue-flatpickr-component";
-import { ref } from "@vue/composition-api";
-import vSelect from "vue-select";
-import Ripple from "vue-ripple-directive";
+import {
+  required,
+  email,
+  confirmed,
+  url,
+  between,
+  alpha,
+  integer,
+  password,
+  min,
+  max,
+  digits,
+  alphaDash,
+  length,
+} from "@validations";
 
 export default {
   components: {
-    BFormSelect,
-    BRow,
     BFormTextarea,
-    BCol,
-    BForm,
-    BFormGroup,
-    flatPickr,
-    BFormInput,
-    vSelect,
+    BFormSelect,
+    BFormDatepicker,
     BFormRadioGroup,
-    BFormCheckboxGroup,
+    ValidationProvider,
+    ValidationObserver,
+    BFormInput,
+    BFormGroup,
+    BForm,
+    BRow,
+    BCol,
     BButton,
   },
-  directives: {
-    Ripple,
-  },
   setup() {
-    const userDataInfo = ref({
-      dob: null,
-      mobile: "+6595895857",
-      website: "https://rowboat.com/insititious/Angelo",
-      language: "French",
-      gender: "female",
-      contactOptions: ["Email", "Message"],
-      addressLine1: "A-65, Belvedere Streets",
-      addressLine2: "",
-      postcode: "",
-      city: "New York",
-      state: "",
-      country: "",
-    });
+    const blankData = {
+      C_DoB: "1992-03-22",
+      C_Adress: "",
+      C_Name: "",
+      C_Email: "",
+      C_Phone: "",
+      C_Note: "",
+    };
 
-    const languageOptions = [
-      "English",
-      "Spanish",
-      "French",
-      "Russian",
-      "German",
-      "Arabic",
-      "Sanskrit",
-    ];
-
-    const genderOptions = [
-      { text: "Male", value: "male" },
-      { text: "Female", value: "female" },
-    ];
-
-    const contactOptionsOptions = ["Email", "Message", "Phone"];
-
-    return {
-      userDataInfo,
-      languageOptions,
-      genderOptions,
-      contactOptionsOptions,
-
-      //select options
+    const objSelectDistrict = {
       selected: null,
       options: [
         { value: null, text: "Please select some item", disabled: true },
@@ -250,10 +245,67 @@ export default {
         { value: "c", text: "This is another option" },
       ],
     };
+
+    const objSelectProvince = {
+      selected: null,
+      options: [
+        { value: null, text: "Please select some item", disabled: true },
+        { value: "a", text: "This is First option" },
+        { value: "b", text: "Default Selected Option" },
+        { value: "c", text: "This is another option" },
+      ],
+    };
+
+    const objSelectWard = {
+      selected: null,
+      options: [
+        { value: null, text: "Please select some item", disabled: true },
+        { value: "a", text: "This is First option" },
+        { value: "b", text: "Default Selected Option" },
+        { value: "c", text: "This is another option" },
+      ],
+    };
+
+    const genderOptions = [
+      { text: "Male", value: "Male" },
+      { text: "Female", value: "Female" },
+    ];
+
+    const validation = {
+      required,
+      confirmed,
+      password,
+      email,
+      min,
+      max,
+      integer,
+      url,
+      alpha,
+      between,
+      digits,
+      length,
+      alphaDash,
+    };
+
+    return {
+      blankData,
+      objSelectDistrict,
+      objSelectProvince,
+      objSelectWard,
+      genderOptions,
+
+      validation,
+    };
+  },
+  methods: {
+    validationForm() {
+      this.$refs.simpleRules.validate().then((success) => {
+        if (success) {
+          // eslint-disable-next-line
+          alert("form submitted!");
+        }
+      });
+    },
   },
 };
 </script>
-
-<style lang="scss">
-@import "@core/scss/vue/libs/vue-flatpicker.scss";
-</style>
