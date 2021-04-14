d1c5dc3b903d8e1419f866e42d18be0db7dd5917
Nguyễn Quang Đức <52225998+Duc-Nguyen99@users.noreply.github.com>
2021-04-14 17:47:58 +0700

    Update UsersListFilters.vue

--- src/views/apps/customers/users-list/UsersListFilters.vue
+++ src/views/apps/customers/users-list/UsersListFilters.vue
@@ -1,55 +1,30 @@
 <template>
   <b-card no-body>
     <b-card-header class="pb-50">
-      <h5>
-        Filters
-      </h5>
+      <h5>Filters</h5>
     </b-card-header>
     <b-card-body>
       <b-row>
-        <b-col
-          cols="12"
-          md="4"
-          class="mb-md-0 mb-2"
-        >
-          <label>Role</label>
+        <b-col cols="12" md="6" class="mb-md-0 mb-2">
+          <label>Groups Customer</label>
           <v-select
             :dir="$store.state.appConfig.isRTL ? 'rtl' : 'ltr'"
             :value="roleFilter"
             :options="roleOptions"
             class="w-100"
-            :reduce="val => val.value"
+            :reduce="(val) => val.value"
             @input="(val) => $emit('update:roleFilter', val)"
           />
         </b-col>
-        <b-col
-          cols="12"
-          md="4"
-          class="mb-md-0 mb-2"
-        >
-          <label>Plan</label>
+        <b-col cols="12" md="6" class="mb-md-0 mb-2">
+          <label>Gender</label>
           <v-select
             :dir="$store.state.appConfig.isRTL ? 'rtl' : 'ltr'"
             :value="planFilter"
             :options="planOptions"
             class="w-100"
-            :reduce="val => val.value"
+            :reduce="(val) => val.value"
             @input="(val) => $emit('update:planFilter', val)"
-          />
-        </b-col>
-        <b-col
-          cols="12"
-          md="4"
-          class="mb-md-0 mb-2"
-        >
-          <label>Status</label>
-          <v-select
-            :dir="$store.state.appConfig.isRTL ? 'rtl' : 'ltr'"
-            :value="statusFilter"
-            :options="statusOptions"
-            class="w-100"
-            :reduce="val => val.value"
-            @input="(val) => $emit('update:statusFilter', val)"
           />
         </b-col>
       </b-row>
@@ -58,10 +33,8 @@
 </template>
 
 <script>
-import {
-  BCard, BCardHeader, BCardBody, BRow, BCol,
-} from 'bootstrap-vue'
-import vSelect from 'vue-select'
+import { BCard, BCardHeader, BCardBody, BRow, BCol } from "bootstrap-vue";
+import vSelect from "vue-select";
 
 export default {
   components: {
@@ -81,10 +54,6 @@ export default {
       type: [String, null],
       default: null,
     },
-    statusFilter: {
-      type: [String, null],
-      default: null,
-    },
     roleOptions: {
       type: Array,
       required: true,
@@ -93,14 +62,10 @@ export default {
       type: Array,
       required: true,
     },
-    statusOptions: {
-      type: Array,
-      required: true,
-    },
   },
-}
+};
 </script>
 
 <style lang="scss">
-@import '@core/scss/vue/libs/vue-select.scss';
+@import "@core/scss/vue/libs/vue-select.scss";
 </style>
