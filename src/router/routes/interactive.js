03d9d4e7cd64d85a7c383323fe86479309f5f351
Vũ Ngọc Tú <vungoctu.dev@gmail.com>
2021-05-01 23:05:20 +0700

    update

--- src/router/routes/interactive.js
+++ src/router/routes/interactive.js
@@ -1,4 +1,35 @@
 export default [
+
+    // *===============================================---*
+    // *--------- VOUCHER ---- ---------------------------------------*
+    // *===============================================---*
+
+    {
+        path: '/apps/group/voucher/list',
+        name: 'apps-group-voucher-list',
+        component: () => import('@/views/apps/voucher/voucher-list/VoucherListGroups.vue'),
+    },
+    {
+        path: '/apps/group/voucher/list/del',
+        name: 'apps-group-voucher-list-del',
+        component: () => import('@/views/apps/voucher/voucher-list/VoucherListGroupsDel.vue'),
+    },
+    {
+        path: '/apps/group/voucher/add',
+        name: 'apps-group-voucher-add',
+        component: () => import('@/views/apps/voucher/voucher-add/VoucherAdd.vue'),
+    },
+    {
+        path: '/apps/group/voucher/view/:id',
+        name: 'apps-group-voucher-view',
+        component: () => import('@/views/apps/voucher/voucher-view/VoucherView.vue'),
+    },
+    {
+        path: '/apps/group/voucher/edit/:id',
+        name: 'apps-group-voucher-edit',
+        component: () => import('@/views/apps/voucher/voucher-edit/VoucherEdit.vue'),
+    },
+
     // *===============================================---*
     // *--------- VOUCHER ---- ---------------------------------------*
     // *===============================================---*
